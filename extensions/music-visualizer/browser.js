9c212eee9df33ca3792d9d34c17567c92ddfd971
Cao Gaoting <caogtaa@gmail.com>
2021-07-10 15:50:30 +0800

    extension basic and i18n

---
+++ extensions/music-visualizer/browser.js
@@ -0,0 +1,19 @@
+"use strict";
+
+exports.methods = {
+    log() {
+        console.log('hello');
+    },
+
+    ExtractFFT() {
+        console.log('extract-fft');
+    }
+};
+
+exports.load = function() {
+
+};
+
+exports.unload = function() {
+
+};
